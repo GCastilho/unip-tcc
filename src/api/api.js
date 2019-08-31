5746fd4c014b6ffe3fe5b312de78d826492c859b
gabriel <gabrielgustavorodrigues@gmail.com>
2019-08-31 02:40:02 -0300

    trash removed
    --no comments

--- src/api/api.js
+++
@@ -1,9 +0,0 @@
-const mongoConnection = require('./mongoConnection')
-const mongoFunctions = require('./mongoFunctions')
-
-mongoConnection.startConnection(function (err){
-    if (err) console.log(err, client )
-    //provavelmente todas as partes do codigo que forem pensar em usar o mongo deverao ser usados aqui dentro
-    //o que provavelmente siginifica o backend inteiro
-    mongoFunctions.insertUser("email", "senha","salt")
-})
