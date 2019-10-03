36ab41664f48a94c195e7e2362a7b77ebe999a84
gabriel <gabrielgustavorodrigues@gmail.com>
2019-10-03 04:46:44 -0300

    criado lista de accounts no mongo
    --collections de accounts é dropada e recriada na inicializaçao do modulo
    --vars trocado pra consts
    --; removidos
    --terminou!!1! corrigido para terminou!!!!

--- external_modules/nano/server.js
+++ external_modules/nano/server.js
@@ -1,36 +1,42 @@
-var express = require("express")
-var bodyParser = require("body-parser");
-var app = express()
-var nanoRcp = require("./rcp.js")
-const axios = require('axios')
+const bodyParser = require("body-parser")
+const nanoRcp = require("./rcp")
+const axios = require("axios")
+const express = require("express")
+const app = express()
+const mongoose = require('mongoose')
 
-/*app.post('handle',function(request,response){
-    var query1=request.body.var1;
-    var query2=request.body.var2;
-});*/
-
-app.get('/', function (req, res) {
-    //TODO: transformar essa funcao em algo que realmente trata o request e o response
-    nanoRcp.createAccount(req, res);
+mongoose.connect('mongodb://127.0.0.1:27017/exchange', {
+	user: 'exchange_server',
+	pass: 'uLCwAJH49ZRzCNW3',
+	useNewUrlParser: true,
+	useCreateIndex: true
 })
+const account = mongoose.model('account', { account: String })
 
-var server = app.listen(50000, function () {
+account.collection.drop(function(err, result){
+//falta um handle em caso de erro, mas acredito que nesse ponto so da erro se: ou a conexao com o banco de dados nao estiver estabelecida
+//ou se ja nao existir uma collection de account
+})
+axios.get('http://localhost:8085/account_list/nano', {
+	responseType: 'stream'
+}).then(({ data }) => {
+	data.on('data', (chunk) => {
+		new account({ account: chunk.toString()}).save()
+	})
+	data.on('end', (chunk) => {
+		console.log('ACABOU!!!!')
+	})
+}).catch((err) => {
+	console.log(err)
+})
+const server = app.listen(50000, function () {
     var host = server.address().address
     var port = server.address().port
    
    console.log("Example app listening at http://%s:%s", host, port)
 })
 
-axios.get('http://localhost:8085/account_list/nano', {
-	responseType: 'stream'
-}).then(({ data }) => {
-	data.on('data', (chunk) => {
-		console.log(`'${chunk.toString()}'`)
-	})
-	data.on('end', (chunk) => {
-		console.log(chunk)
-		console.log('ACABOU!!1!')
-	})
-}).catch((err) => {
-	console.log(err)
+app.get('/newAccount/', function (req, res) {
+    //TODO: transformar essa funcao em algo que realmente trata o request e o response
+    nanoRcp.createAccount(req, res)
 })
