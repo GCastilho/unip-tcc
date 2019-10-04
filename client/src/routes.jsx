080d912f527accf24ae1dd1b84d2e9edda101435
João Victor <joaojvdsvictor@gmail.com>
2019-10-04 11:38:46 -0300

    Criado paginas main e not found
    --Criado arquivos das paginas main e not found
    --Configurado rotas no arquivo router

--- client/src/routes.jsx
+++ client/src/routes.jsx
@@ -2,14 +2,18 @@ import React from 'react';
 import ReactLoading from 'react-loading';
 import {Route, Switch} from 'react-router-dom'
 
+const Main = React.lazy(() => import('./pages/main/Main'));
 const Login = React.lazy(() => import('./pages/login/Login'));
 const Register = React.lazy(() => import('./pages/register/Register'));
+const NotFound = React.lazy(() => import('./pages/notFound/NotFound'))
 
 export default () => (
     <React.Suspense fallback={<ReactLoading className='loading' type='spinningBubbles' color='#fff'/>}>
         <Switch>
+            <Route exact path='/'><Main/></Route>
             <Route path='/login'><Login/></Route>
             <Route path='/register'><Register/></Route>
+            <Route path='*'><NotFound/></Route>
         </Switch>
     </React.Suspense>
 )
