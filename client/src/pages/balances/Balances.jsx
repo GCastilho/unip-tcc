1cb162f392191a8cb7d357c27e88529645f3912e
Lucas <cmarottol@hotmalil.com>
2019-10-12 15:22:25 -0300

    Clear code
    -- Retirado codigo inutilzado

--- client/src/pages/balances/Balances.jsx
+++ client/src/pages/balances/Balances.jsx
@@ -14,28 +14,7 @@ import BalancesTableItem from "../../components/BalancesTableItem/BalancesTableI
 let simu = [
     { code: "ETH", name: "Etherium", value: "0.00000000" },
     { code: "BTC", name: "Bitcoin", value: "0.00000000" },
-    { code: "NANO", name: "NANO", value: "0.00000000" },
-    { code: "USD", name: "Dollar", value: "0.00000000" },
-    { code: "USDT", name: "Tether", value: "0.00000000" },
-    { code: "LTC", name: "Litecoin", value: "0.00000000" },
-    { code: "ETH1", name: "Etherium 1", value: "0.00000000" },
-    { code: "BTC1", name: "Bitcoin 1", value: "0.00000000" },
-    { code: "NANO1", name: "NANO 1", value: "0.00000000" },
-    { code: "USD1", name: "Dollar 1", value: "0.00000000" },
-    { code: "USDT1", name: "Tether 1", value: "0.00000000" },
-    { code: "LTC1", name: "Litecoin 1", value: "0.00000000" },
-    { code: "ETH2", name: "Etherium 2", value: "0.00000000" },
-    { code: "BTC2", name: "Bitcoin 2", value: "0.00000000" },
-    { code: "NANO2", name: "NANO 2", value: "0.00000000" },
-    { code: "USD2", name: "Dollar 2", value: "0.00000000" },
-    { code: "USDT2", name: "Tether 2", value: "0.00000000" },
-    { code: "LTC2", name: "Litecoin 2", value: "0.00000000" },
-    { code: "ETH3", name: "Etherium 3", value: "0.00000000" },
-    { code: "BTC3", name: "Bitcoin 3", value: "0.00000000" },
-    { code: "NANO3", name: "NANO 3", value: "0.00000000" },
-    { code: "USD3", name: "Dollar 3", value: "0.00000000" },
-    { code: "USDT3", name: "Tether 3", value: "0.00000000" },
-    { code: "LTC3", name: "Litecoin 3", value: "0.00000000" }
+    { code: "NANO", name: "NANO", value: "0.00000000" }
 ];
 
 export default class Balances extends Component {
@@ -57,7 +36,7 @@ export default class Balances extends Component {
         //need to create a request in here from server
         let recieveData = simu;
         recieveData.forEach(bal => {
-            if (bal !== undefined) {
+            if (bal) {
                 let newState = this.state;
                 newState.balances.push(bal);
                 this.setState(newState);
@@ -69,28 +48,15 @@ export default class Balances extends Component {
      * @description append new data or replace existing one
      * @param {JSON} balance recieve the item for the balance ex. {code,name,balance}
      */
-    appendBalance = (balance) => {
+    appendBalance = ({ balance, code }) => {
         let newState = this.state;
         for (let i = 0; i < newState.balances.length; i++) {
-            if (newState.balances[i].code === balance.code) {
-                newState.balances[i] = balance;
+            if (newState.balances[i].code === code) {
+                newState.balances[i].balance = balance;
             }
         }
         this.setState(newState);
         console.log(newState);
-
-        /*
-        let tr = document.getElementById(balance.code + "_value");
-        if (tr !== undefined && tr !== null) {
-            tr.innerHTML = balance.value;
-        } else {
-            let tb = document.getElementById("balancesList");
-            if (tb !== undefined) {
-                tb.appendChild(this.newRow(balance));
-            }
-
-        }
-        //*/
     }
     /**
      * @description method called from client to make an deposit
@@ -136,11 +102,17 @@ export default class Balances extends Component {
                         <th className="balances-th">Name</th>
                         <th colSpan='2' className="balances-th">Balance</th>
                     </tr>
-                    {this.state.balances.map(bal => (<BalancesTableItem key={bal.name} code={bal.code} name={bal.name} value={bal.value}
-                        depositOnClick={() => (this.depositBalance(bal))} withdrawOnClick={() => (this.withdrawBalance(bal))} />))}
+                    {this.state.balances.map(bal => (
+                        <tr>
+                            <td>{bal.code}</td>
+                            <td>{bal.name}</td>
+                            <td>{bal.value}</td>
+                            <td className="balances-td"><div type='button' onClick={() => (this.depositBalance(bal))}>Deposit</div></td>
+                            <td className="balances-td"><div type='button' onClick={() => (this.withdrawBalance(bal))}>Withdraw</div></td>
+                        </tr>
+                    ))}
                 </tbody>
             </table >
         )
     }
-
 }
