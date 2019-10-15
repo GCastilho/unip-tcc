31ef1d0c8dca52d60d2653c121a0304541ff25d1
GCastilho <castilho.gabriel14@gmail.com>
2019-10-15 20:55:41 -0300

    Adicionado comando 'ping' no listener
    -- O módulo externo agora checa se o servidor está online antes de requisitar as accounts da NANO e só faz a requisição uma vez que o servidor responder
    -- Melhorada as mensagens de startup do módulo externo
    -- Reduzido span de connection refused do websocket da NANO
    -- Adicionado comando 'ping' no listener, que responde 'pong'

---
+++ src/currencyApi/listener/functions/ping.js
@@ -0,0 +1,10 @@
+/*
+ * src/currencyApi/listener/functions/ping.js
+ * 
+ * Retorna pong
+ */
+
+module.exports = function ping() {
+	/** @todo Não considerar como 'modulo online' até checar se o servidor responde PING da porta correta */
+	return 'pong'
+}
