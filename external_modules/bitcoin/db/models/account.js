cb7e74da7153ed40bde6854b0f2b268810570149
gabr1gus <gabrielgustavorodrigues@gmail.com>
2019-10-17 20:03:00 -0300

    inicio da api do bitcoin
    --copiado os arquivos comuns entre bitcoin e nano(depois esses irão ser organizados para uma pasta common)
    --criacao de um prototipo pro armazenamento das transactions

---
+++ external_modules/bitcoin/db/models/account.js
@@ -0,0 +1,12 @@
+const mongoose = require('../mongoose')
+
+const transaction = mongoose.model('transaction', {
+	tx: {
+		type: String,
+		trim: true,
+		unique: true,
+		required: true
+	}
+})
+
+module.exports = transaction
