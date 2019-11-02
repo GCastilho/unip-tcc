a8ecbe9b6cd5ae021219bd2b96eb9a81ef9d45e1
GCastilho <castilho.gabriel14@gmail.com>
2019-11-02 02:16:42 -0300

    webdocket agora é definido em ser arquivo próprio
    -- A const io do websocket foi separada do app e do server.js e agora é definida em seu módulo próprio
    -- Adaptado código para seguir o padrão do eslint
    -- Temporariamente removido a external_modules e a web-jsnative do eslint
    -- Tudo parece ainda estar funcionando

--- src/websocket/api-loader.js
+++ src/websocket/api-loader.js
@@ -3,56 +3,56 @@
  *
  * Inicializador do modulo API do WebSocket
  */
-const fs = require('fs');
-const path = require('path');
+const fs = require('fs')
+const path = require('path')
 
-const subpath = "api/";
+const subpath = 'api/'
 
 module.exports = function (socket) {
 
-    /**
-     * cria uma instancia de maping para sustentar as rotas
-     */
-    if (!socket.rotas) {
-        socket.rotas = new Map();
-    }
-    socket.enableLog = true; //abilita o log de carregamento das rotas no console
+	/**
+	 * cria uma instancia de maping para sustentar as rotas
+	 */
+	if (!socket.rotas) {
+		socket.rotas = new Map()
+	}
+	socket.enableLog = true //abilita o log de carregamento das rotas no console
 
-    /**
-     * Carrega toda a lista de diretorios da pasta /api
-     * e carrega cada sub rota de cada versão da api
-     */
+	/**
+	 * Carrega toda a lista de diretorios da pasta /api
+	 * e carrega cada sub rota de cada versão da api
+	 */
 
-    fs.readdirSync(path.join(__dirname + "/api/")).forEach(apiVersion => {
-        let subLoader = path.join(__dirname + "/api/" + apiVersion + "/index.js");
-        if (fs.existsSync(subLoader)) {
-            require(subLoader)(socket, subpath);
-        }
-    })
+	fs.readdirSync(path.join(__dirname + '/api/')).forEach(apiVersion => {
+		let subLoader = path.join(__dirname + '/api/' + apiVersion + '/index.js')
+		if (fs.existsSync(subLoader)) {
+			require(subLoader)(socket, subpath)
+		}
+	})
 
-    /**
-     * instancia o handler de rotas
-     */
-    socket.on("api", request => {
-        /**
-         * JSON com o seguinte formato para o request
-         * {route:"api/v1.0/...", data:{...}}
-         * "route" sendo a rota de chamamento
-         * "data" os dados para um call ou um objeto vazio se for um get simples
-         */
-        if (request["route"] && request["data"]) {//checa a estrutura do json
-            if (socket.rotas.has(request["route"])) {
-                request = socket.rotas.get(request["route"])(request);
-            } else {
-                request["status"] = "error";
-                request["error"] = "could not identify route";
-            }
-        } else {
-            /**
-             * se a estrutura estiver invalida ele retorna um erro e o request enviado
-             */
-            request = { status: "error", error: "wrong request structure", request: request };
-        }
-        socket.emit("api", request);
-    })
+	/**
+	 * instancia o handler de rotas
+	 */
+	socket.on('api', request => {
+		/**
+		 * JSON com o seguinte formato para o request
+		 * {route:"api/v1.0/...", data:{...}}
+		 * "route" sendo a rota de chamamento
+		 * "data" os dados para um call ou um objeto vazio se for um get simples
+		 */
+		if (request['route'] && request['data']) {//checa a estrutura do json
+			if (socket.rotas.has(request['route'])) {
+				request = socket.rotas.get(request['route'])(request)
+			} else {
+				request['status'] = 'error'
+				request['error'] = 'could not identify route'
+			}
+		} else {
+			/**
+			 * se a estrutura estiver invalida ele retorna um erro e o request enviado
+			 */
+			request = { status: 'error', error: 'wrong request structure', request: request }
+		}
+		socket.emit('api', request)
+	})
 }
