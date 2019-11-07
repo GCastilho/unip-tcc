4694acc4953ce35cc5ae3f1088c5744cf5cb5d49
GCastilho <castilho.gabriel14@gmail.com>
2019-11-07 02:54:09 -0300

    Adicionado handler para withdraw no socket

--- src/websocket/api/v1_0/balances/_withdraw.js
+++ src/websocket/api/v1_0/balances/_withdraw.js
@@ -2,16 +2,15 @@
  * src/websocket/api/v1_0/teste/_withdraw.js
  * rota de saque de moedas
  */
+
+const currencyApi = require('../../../../currencyApi')
+
 module.exports = function (socket, upRota) {
 	if (socket.enableLog) console.log('-- criado rota: ' + upRota + 'get')
-	socket.rotas.set(upRota + 'get', function (request) {
-		/**
-         * TODO: Withdraw
-         */
+	socket.rotas.set(upRota + 'get', async function (request) {
+		const { email, currency, address, amount } = request.data
+		await currencyApi.withdraw(email, currency, address, amount)
 
-		/**
-         * retorna o request modificado com as informações requisitadas
-         */
 		return request// é necessario retornar o request;
 	})
 }
