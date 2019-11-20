e54245b40ae5ac10edd67ddbd7ea35a5e660e93b
GCastilho <castilho.gabriel14@gmail.com>
2019-11-20 18:56:38 -0300

    Reativado validador de accounts
    -- Modificado e reativado o validador de accounts de nano e bitcoin (usando o WAValidator), uma account inválida agora seja corretamente rejeitada pelo banco de dados e não será salva

--- src/db/models/currencies/bitcoin.ts
+++ src/db/models/currencies/bitcoin.ts
@@ -1,13 +1,14 @@
-// import * as WAValidator from 'multicoin-address-validator'
+import * as WAValidator from 'multicoin-address-validator'
 import { CurrencySchema } from './common'
 import { Schema } from 'mongoose'
 
-const Bitcoin: Schema = new Schema({...CurrencySchema.obj, ...{
-	account: {
-		// validate: [function bitcoin_account_validator(accounts: string[]) {
-		// 	return accounts.every((account) => WAValidator.validate(account, 'bitcoin', 'testnet'))
-		// }, 'Invalid bitcoin account address']
-	}
-}})
+CurrencySchema.obj.accounts.validate = {
+	validator: function bitcoin_account_validator(accounts: string[]) {
+		return accounts.every((account) => WAValidator.validate(account, 'bitcoin', 'testnet'))
+	},
+	message: 'Invalid bitcoin account address'
+}
+
+const Bitcoin: Schema = new Schema(CurrencySchema.obj)
 
 export = Bitcoin
--- src/db/models/currencies/nano.ts
+++ src/db/models/currencies/nano.ts
@@ -1,13 +1,14 @@
-// import * as WAValidator from 'multicoin-address-validator'
+import * as WAValidator from 'multicoin-address-validator'
 import { CurrencySchema } from './common'
 import { Schema } from 'mongoose'
 
-const Nano: Schema = new Schema({...CurrencySchema.obj, ...{
-	account: {
-		// validate: [function nano_account_validator(accounts: string[]) {
-		// 	return accounts.every((account) => WAValidator.validate(account, 'nano', 'testnet'))
-		// }, 'Invalid bitcoin account address']
-	}
-}})
+CurrencySchema.obj.accounts.validate = {
+	validator: function nano_account_validator(accounts: string[]) {
+		return accounts.every((account) => WAValidator.validate(account, 'nano', 'testnet'))
+	},
+	message: 'Invalid nano account address'
+}
+
+const Nano: Schema = new Schema(CurrencySchema.obj)
 
 export = Nano
