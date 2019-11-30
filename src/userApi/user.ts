3c379fbc49a939d6082befea6e4519a94bd78085
GCastilho <castilho.gabriel14@gmail.com>
2019-11-30 01:24:22 -0300

    Adicionado função cancelPending na userApi
    -- Adicionado função cancelPending na User da userApi, que cancela uma operação pendente (agendada pelo addPending) e restaura os saldos ao seu valor original

--- src/userApi/user.ts
+++ src/userApi/user.ts
@@ -1,3 +1,4 @@
+import mongoose from 'mongoose'
 import { sha512 } from 'js-sha512'
 import { Person } from '../db/models/person/interface'
 import { Pending } from '../db/models/person/currencies/interface'
@@ -98,5 +99,76 @@ export default class User {
 
 		if (!response.lastErrorObject.updatedExisting)
 			throw 'NotEnoughFunds'
+	}
+
+	/**
+	 * Cancela uma operação adicionada pelo 'addPending', voltando tudo ao seu
+	 * estado original
+	 * 
+	 * Essa função faz duas consultas no banco de dados (a primeira é para
+	 * detectar como a operação de cancelamento deve ocorrer e a segunda é a
+	 * operação de cancelamento de fato), a atualização é async safe
+	 * 
+	 * @param currency A currency que a operação se refere
+	 * @param opid O ObjectId que referencia o documento da operação em sua
+	 * respectiva collection
+	 * 
+	 * @throws OperationNotFound if an operation was not found for THIS user
+	 * 
+	 * Se a operação for removida entre a primeira ou a segunda consulta (por
+	 * exemplo, caso ela seja executada), a operação de cancelamento vai falhar
+	 * com um 'OperationNotFound' que será disparado pela segunda query
+	 */
+	cancelPending = async (currency: string, opid: mongoose.Types.ObjectId): Promise<void> => {
+		let amount: Pending['amount']
+		/**
+		 * Pega o amount da operação a ser cancelada; É necessário saber esse
+		 * valor para saber como a remoção da operação deverá ser feita
+		 */
+		{
+			// No primeiro item do array retornado pega o objeto 'item'
+			const [{ item }] = await this.person.collection.aggregate([
+				{
+					$match: { _id: this.person._id }
+				}, {
+					$limit: 1
+				}, {
+					$project: {
+						item: {
+							$filter: {
+								input: `$currencies.${currency}.pending`,
+								as: 'item',
+								cond: {
+									$eq: [ `$$item.opid`, opid ]
+								}
+							}
+						}
+					}
+				}
+			]).toArray()
+			if (!item[0] || !item[0].amount)
+				throw 'OperationNotFound'
+			amount = item[0].amount
+		}
+
+		// Se tudo for ficar numa função 'pending', isso fica no closure dela
+		const balanceObj = `currencies.${currency}._balance`
+
+		// Remove a operation do array e atualiza os saldos
+		const response = await this.person.collection.findOneAndUpdate({
+			_id: this.person._id,
+			[`currencies.${currency}.pending.opid`]: opid
+		}, {
+			$inc: {
+				[`${balanceObj}.locked`]: - Math.abs(amount),
+				[`${balanceObj}.available`]: amount < 0 ? Math.abs(amount) : 0
+			},
+			$pull: {
+				[`currencies.${currency}.pending`]: { opid }
+			}
+		})
+
+		if (!response)
+			throw 'OperationNotFound'
 	}
 }
