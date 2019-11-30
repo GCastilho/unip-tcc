2de99506a12fa671040275ff481a371bd61253d6
GCastilho <castilho.gabriel14@gmail.com>
2019-11-30 15:28:54 -0300

    Refatorado métodos pending em um submétodo 'balanceOp'
    -- As 3 operações, addPending, cancelPending e completePending agora são add, cancel e complete e estão no pseudométodo 'balanceOp'
    -- Refatorado as funções para redizir a escrita de código duplicado

--- src/userApi/user.ts
+++ src/userApi/user.ts
@@ -60,184 +60,201 @@ export default class User {
 	}
 
 	/**
-	 * Adiciona uma operação de mudança de saldo pendente no array 'pending' da
-	 * currency trava o saldo utilizado na operação
+	 * Contém métodos para a manipulação de operações de mudança de saldo
 	 * 
-	 * Caso seja uma operação de redução de saldo e available - amount >= 0, o
-	 * valor de 'amount' será retirado de 'available' e adicionado em 'locked'.
-	 * Se a operação for de aumento de saldo, a única coisa que será feita é
-	 * aumentar o valor do locked
-	 * 
-	 * Essa função é async safe, ou seja, a operação é feita de forma atômica
-	 * no banco de dados
-	 * 
-	 * @param currency A currency que a operação se refere
-	 * @param op O objeto da operação pendente que será adicionado
-	 * 
-	 * @throws NotEnoughFunds Caso não haja saldo disponível (o campo
-	 * 'available' do balance) para executar a operação
+	 * Todos os métodos são async safe
 	 */
-	addPending = async (currency: string, op: Pending): Promise<void> => {
-		const balanceObj = `currencies.${currency}._balance`
-
-		const response = await this.person.collection.findOneAndUpdate({
-			_id: this.person._id,
-			$expr: {
-				$gte: [
-					{ $add: [`$${balanceObj}.available`, op.amount] }, 0
-				]
-			}
-		}, {
-			$inc: {
-				[`${balanceObj}.locked`]: Math.abs(op.amount),
-				[`${balanceObj}.available`]: op.amount < 0 ? op.amount : 0
-			},
-			$push: {
-				[`currencies.${currency}.pending`]: op
-			}
-		})
-
-		if (!response.lastErrorObject.updatedExisting)
-			throw 'NotEnoughFunds'
-	}
-
-	/**
-	 * Cancela uma operação adicionada pelo 'addPending', voltando tudo ao seu
-	 * estado original
-	 * 
-	 * Essa função é async safe
-	 * 
-	 * @param currency A currency que a operação se refere
-	 * @param opid O ObjectId que referencia o documento da operação em sua
-	 * respectiva collection
-	 * 
-	 * @throws OperationNotFound if an operation was not found for THIS user
-	 * 
-	 * Essa função faz duas consultas ao banco de dados, então se a operação for
-	 * removida entre a primeira e a segunda consulta (por exemplo, caso ela
-	 * seja executada), a operação de cancelamento vai falhar com um
-	 * 'OperationNotFound' que será disparado pela segunda query
-	 */
-	cancelPending = async (currency: string, opid: mongoose.Types.ObjectId): Promise<void> => {
-		let amount: Pending['amount']
+	balanceOp = (() => {
 		/**
-		 * Pega o amount da operação a ser cancelada; É necessário saber esse
-		 * valor para saber como a remoção da operação deverá ser feita
+		 * Adiciona uma operação de mudança de saldo pendente no array 'pending'
+		 * da currency trava o saldo utilizado na operação
+		 * 
+		 * Caso seja uma operação de redução de saldo e available - amount >= 0,
+		 * o valor de 'amount' será retirado de 'available' e adicionado em
+		 * 'locked'. Se a operação for de aumento de saldo, a única coisa que
+		 * será feita é aumentar o valor do locked
+		 * 
+		 * Essa função é async safe, ou seja, a operação é feita de forma
+		 * atômica no banco de dados
+		 * 
+		 * @param currency A currency que a operação se refere
+		 * @param op O objeto da operação pendente que será adicionado
+		 * 
+		 * @throws NotEnoughFunds Caso não haja saldo disponível (o campo
+		 * 'available' do balance) para executar a operação
 		 */
-		{
-			// No primeiro item do array retornado pega o objeto 'item'
-			const [{ item }] = await this.person.collection.aggregate([
+		const add = async (currency: string, op: Pending): Promise<void> => {
+			const balanceObj = `currencies.${currency}._balance`
+	
+			const response = await this.person.collection.findOneAndUpdate({
+				_id: this.person._id,
+				$expr: {
+					$gte: [
+						{ $add: [`$${balanceObj}.available`, op.amount] }, 0
+					]
+				}
+			}, {
+				$inc: {
+					[`${balanceObj}.locked`]: Math.abs(op.amount),
+					[`${balanceObj}.available`]: op.amount < 0 ? op.amount : 0
+				},
+				$push: {
+					[`currencies.${currency}.pending`]: op
+				}
+			})
+	
+			if (!response.lastErrorObject.updatedExisting)
+				throw 'NotEnoughFunds'
+		}
+
+		/**
+		 * Retorna o amount de uma operação DESSE usuário
+		 * 
+		 * @param currency A currency que a operação se refere
+		 * @param opid O ObjectId que referencia o documento da operação em sua
+		 * respectiva collection
+		 * 
+		 * @throws OperationNotFound if an operation was not found for THIS user
+		 */
+		const _getOpAmount = async (
+			currency: string,
+			opid: mongoose.Types.ObjectId
+		): Promise<number> => {
+			// No primeiro item do array retornado pega o objeto 'operations'
+			const [{ operations }] = await this.person.collection.aggregate([
 				{
 					$match: { _id: this.person._id }
 				}, {
 					$limit: 1
 				}, {
 					$project: {
-						item: {
+						operations: {
 							$filter: {
 								input: `$currencies.${currency}.pending`,
-								as: 'item',
+								as: 'operations',
 								cond: {
-									$eq: [ `$$item.opid`, opid ]
+									$eq: [ `$$operations.opid`, opid ]
 								}
 							}
 						}
 					}
 				}
 			]).toArray()
-			if (!item[0] || !item[0].amount)
+			if (!operations[0] || typeof operations[0].amount != 'number')
 				throw 'OperationNotFound'
-			amount = item[0].amount
+
+			return operations[0].amount
 		}
 
-		// Se tudo for ficar numa função 'pending', isso fica no closure dela
-		const balanceObj = `currencies.${currency}._balance`
-
-		// Remove a operation do array e atualiza os saldos
-		const response = await this.person.collection.findOneAndUpdate({
-			_id: this.person._id,
-			[`currencies.${currency}.pending.opid`]: opid
-		}, {
-			$inc: {
-				[`${balanceObj}.locked`]: - Math.abs(amount),
-				[`${balanceObj}.available`]: amount < 0 ? Math.abs(amount) : 0
-			},
-			$pull: {
-				[`currencies.${currency}.pending`]: { opid }
-			}
-		})
-
-		if (!response)
-			throw 'OperationNotFound'
-	}
-
-	/**
-	 * Completa uma operação pendente, atualizando os saldos e removendo a
-	 * operation do array de 'pending'
-	 * 
-	 * Essa função é async safe
-	 * 
-	 * @param currency A currency que a operação se refere
-	 * @param opid O ObjectId que referencia o documento da operação em sua
-	 * respectiva collection
-	 * 
-	 * @throws OperationNotFound if an operation was not found for THIS user
-	 * 
-	 * Essa função faz duas consultas ao banco de dados, então se a operação for
-	 * removida entre a primeira e a segunda consulta (por exemplo, caso ela
-	 * seja cancelada), a operação de encerramento vai falhar com um
-	 * 'OperationNotFound' que será disparado pela segunda query
-	 */
-	completePending = async (currency: string, opid: mongoose.Types.ObjectId): Promise<void> => {
-		let amount: Pending['amount']
 		/**
-		 * Pega o amount da operação a ser cancelada; É necessário saber esse
-		 * valor para saber como a remoção da operação deverá ser feita
+		 * Remove uma operação do banco de dados, subtraindo o módulo do
+		 * opAmount do campo locked e incrementando o campo 'available' com o
+		 * valor passado em 'changeInAvailable'
+		 * 
+		 * @param currency A currency que a operação se refere
+		 * @param opid O ObjectId que referencia o documento da operação em sua
+		 * respectiva collection
+		 * @param opAmount O amount bruto da operação
+		 * @param changeInAvailable O valor que o campo available deve ser
+		 * incrementado
+		 * 
+		 * @throws OperationNotFound if an operation was not found for THIS user
 		 */
-		{
-			// No primeiro item do array retornado pega o objeto 'item'
-			const [{ item }] = await this.person.collection.aggregate([
-				{
-					$match: { _id: this.person._id }
-				}, {
-					$limit: 1
-				}, {
-					$project: {
-						item: {
-							$filter: {
-								input: `$currencies.${currency}.pending`,
-								as: 'item',
-								cond: {
-									$eq: [ `$$item.opid`, opid ]
-								}
-							}
-						}
-					}
+		const _removeOperation = async(
+			currency: string,
+			opid: mongoose.Types.ObjectId,
+			opAmount: number,
+			changeInAvailable: number
+		): Promise<void> => {
+			const balanceObj = `currencies.${currency}._balance`
+
+			const response = await this.person.collection.findOneAndUpdate({
+				_id: this.person._id,
+				[`currencies.${currency}.pending.opid`]: opid
+			}, {
+				$inc: {
+					[`${balanceObj}.locked`]: - Math.abs(opAmount),
+					[`${balanceObj}.available`]: changeInAvailable
+				},
+				$pull: {
+					[`currencies.${currency}.pending`]: { opid }
 				}
-			]).toArray()
-			if (!item[0] || !item[0].amount)
+			})
+	
+			if (!response)
 				throw 'OperationNotFound'
-			amount = item[0].amount
 		}
 
-		// Se tudo for ficar numa função 'pending', isso fica no closure dela
-		const balanceObj = `currencies.${currency}._balance`
+		/**
+		 * Cancela uma operação adicionada pelo 'addPending', voltando tudo ao
+		 * seu estado original
+		 * 
+		 * Essa função é async safe
+		 * 
+		 * @param currency A currency que a operação se refere
+		 * @param opid O ObjectId que referencia o documento da operação em sua
+		 * respectiva collection
+		 * 
+		 * @throws OperationNotFound if an operation was not found for THIS user
+		 */
+		const cancel = async (
+			currency: string,
+			opid: mongoose.Types.ObjectId
+		): Promise<void> => {
+			/**
+			 * O amount da operação
+			 */
+			const amount: Pending['amount'] = await _getOpAmount(currency, opid)
 
-		// Remove a operation do array e atualiza os saldos
-		const response = await this.person.collection.findOneAndUpdate({
-			_id: this.person._id,
-			[`currencies.${currency}.pending.opid`]: opid
-		}, {
-			$inc: {
-				[`${balanceObj}.locked`]: - Math.abs(amount),
-				[`${balanceObj}.available`]: amount < 0 ? 0 : amount
-			},
-			$pull: {
-				[`currencies.${currency}.pending`]: { opid }
-			}
-		})
+			/**
+			 * Calcula o quanto o campo 'available' deverá ser alterado para
+			 * retornar os saldos ao estado original
+			 */
+			const changeInAvailable = amount < 0 ? Math.abs(amount) : 0
 
-		if (!response)
-			throw 'OperationNotFound'
-	}
+			/**
+			 * Remove a operação pendente e volta os saldos ao estado original
+			 */
+			await _removeOperation(currency, opid, amount, changeInAvailable)
+		}
+
+		/**
+		 * Completa uma operação pendente, atualizando os saldos e removendo a
+		 * operation do array de 'pending'
+		 * 
+		 * Essa função é async safe
+		 * 
+		 * @param currency A currency que a operação se refere
+		 * @param opid O ObjectId que referencia o documento da operação em sua
+		 * respectiva collection
+		 * 
+		 * @throws OperationNotFound if an operation was not found for THIS user
+		 */
+		const complete = async (
+			currency: string,
+			opid: mongoose.Types.ObjectId
+		): Promise<void> => {
+			/**
+			 * O amount da operação
+			 */
+			const amount: Pending['amount'] = await _getOpAmount(currency, opid)
+
+			/**
+			 * Calcula o quanto o campo 'available' deverá ser alterado para
+			 * completar a operação
+			 */
+			const changeInAvailable = amount < 0 ? 0 : amount
+
+			/**
+			 * Remove a operação pendente e atualiza os saldos
+			 */
+			await _removeOperation(currency, opid, amount, changeInAvailable)
+		}
+
+		return {
+			add,
+			cancel,
+			complete
+		}
+	})()
 }
