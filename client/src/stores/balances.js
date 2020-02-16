46ddbe1476bba621738587f6c7c88e21b560a676
GCastilho <castilho.gabriel14@gmail.com>
2020-02-16 00:29:41 -0300

    Melhorado o funcionamento do websocket
    -- A promessa retornada pela função emit do websocket agora só irá resolver caso o socket esteja conectado ou uma vez que ele se conectar, ou seja, o evento só será emitido no socket caso ele esteja conectado
    -- A função emit não rejeta mais um erro "SocketDisconnected"
    -- Refatorado a store de blances para o novo funcionamento do websocket

--- client/src/stores/balances.js
+++ client/src/stores/balances.js
@@ -1,5 +1,5 @@
 import { writable } from 'svelte/store'
-import { emit, addSocketListener, removeSocketListner } from '../websocket'
+import { emit } from '../websocket'
 import * as auth from '../stores/auth'
 
 const { subscribe, set, update } = writable({})
@@ -9,34 +9,15 @@ const { subscribe, set, update } = writable({})
  */
 export { subscribe, set, update }
 
-/** Indica se autenticado ou não (valor da store de auth) */
-let authenticated = false
-
 /**
- * Atualiza o saldo da store de balances com os dados do servidor
- * 
- * Caso o socket esteja desconectado adiciona um listener para tentar de novo
- * assim que a conexão for estabelecida
+ * Ao autenticar com o socket atualiza o saldo com o valor do servidor
  */
-async function fetchBalance() {
-	if (!authenticated) return
+auth.subscribe(async auth => {
+	if (!auth) return
 	try {
 		const balances = await emit('fetch_balances')
 		set(balances)
 	} catch(err) {
-		if (err === 'SocketDisconnected') {
-			return addSocketListener('connect', fetchBalance)
-		} else {
-			console.error('Error while fething balances:', err)
-		}
+		console.error('Error while fething balances:', err)
 	}
-	removeSocketListner('connect', fetchBalance)
-}
-
-/**
- * Ao autenticar com o socket atualiza o saldo com o valor do servidor
- */
-auth.subscribe(auth => {
-	authenticated = auth
-	fetchBalance()
 })
--- client/src/websocket.js
+++ client/src/websocket.js
@@ -19,6 +19,17 @@ export function route(path) {
 }
 
 /**
+ * Checa a conexão; A promessa retornada por essa função será resolvida caso
+ * o socket esteja conectado ou uma vez que ele se conectar
+ */
+function checkConnection() {
+	return new Promise(resolve => {
+		if (socket.connected) return resolve()
+		socket.once('connect', resolve)
+	})
+}
+
+/**
  * Emite um evento ao socket, com o último argumento sendo uma função callback.
  * O retorno dessa função callback é retornado na forma de uma promessa,
  * rejeitada ou resolvida de acordo com a chamada desse callback
@@ -28,13 +39,14 @@ export function route(path) {
  */
 export function emit(event, ...args) {
 	return new Promise((resolve, reject) => {
-		if (socket.disconnected) return reject('SocketDisconnected')
-		socket.emit(event, ...args, ((error, response) => {
-			if (error)
-				reject(error)
-			else
-				resolve(response)
-		}))
+		checkConnection().then(() => {
+			socket.emit(event, ...args, ((error, response) => {
+				if (error)
+					reject(error)
+				else
+					resolve(response)
+			}))
+		})
 	})
 }
 
