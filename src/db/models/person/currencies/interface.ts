9048d415606ed628e79e7efc4eceecabedbbd4c3
GCastilho <castilho.gabriel14@gmail.com>
2020-02-23 20:53:23 -0300

    Movido CurrencySchema para um arquivo próprio
    -- O schema e interface da Currency (genérica) foi movido para um arquivo separado 'generic'

--- src/db/models/person/currencies/interface.ts
+++ src/db/models/person/currencies/interface.ts
@@ -1,19 +1,4 @@
-import { Decimal128 } from 'mongodb'
-import { Pending } from './pending'
-
-/** A interface de uma currency da colletion people */
-interface Currency {
-	balance: {
-		/** Saldo disponível para operações */
-		available: Decimal128
-		/** Saldo bloqueado em operações */
-		locked: Decimal128
-	}
-	/** Array de accounts de <currency> do usuário */
-	accounts: string[]
-	/** Operações pendentes que involvem alteração de saldo */
-	pending: Pending[]
-}
+import type { Currency } from './generic'
 
 /**
  * A interface do sub-documento 'currencies' da collection people
--- src/db/models/person/currencies/schemas.ts
+++ src/db/models/person/currencies/schemas.ts
@@ -4,41 +4,7 @@
 
 import { Schema } from 'mongoose'
 import * as validators from './validators'
-import { Decimal128 } from 'mongodb'
-import { PendingSchema } from './pending'
-
-/**
- * Schema de cada uma das currencies da collection people, haverá um
- * sub-documento desse schema para cada currency
- */
-const CurrencySchema: Schema = new Schema({
-	balance: {
-		available: {
-			type: Decimal128,
-			default: 0,
-			min: [0, 'Available balance can not be less than 0']
-		},
-		locked: {
-			type: Decimal128,
-			default: 0,
-			min: [0, 'Locked balance can not be less than 0']
-		}
-	},
-	accounts: {
-		type: [String],
-		/**
-		 * @todo Como o sparse não funciona em indices compostos, fazer uma
-		 * função de validação personalizada que verifica se o novo endereço
-		 * é de fato único
-		 */
-		// sparse: true,
-		// unique: true,
-		trim: true
-	},
-	pending: {
-		type: [PendingSchema]
-	}
-})
+import { CurrencySchema } from './generic'
 
 /*
  * Adiciona a função de validação de address na currency e monta o novo schema
