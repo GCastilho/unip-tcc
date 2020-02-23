ee1830f72bfa32a0f60210f2521bf35c56bf11c4
GCastilho <castilho.gabriel14@gmail.com>
2020-02-23 19:50:09 -0300

    Schema movido pra index
    -- O schema da Person foi movido para o index
    -- O export do model agora é no padrão ES6, usando default

--- src/db/models/person/index.ts
+++ src/db/models/person/index.ts
@@ -1,9 +1,33 @@
-import mongoose from 'mongoose'
-import { Person } from './interface'
-import { PersonSchema } from './schema'
+import mongoose, { Schema } from 'mongoose'
+import currenciesSchema from './currencies'
+import type { Person } from './interface'
+
+/**
+ * Schema da collection de usuários (people)
+ */
+const PersonSchema: Schema = new Schema({
+	email: {
+		type: String,
+		trim: true,
+		lowercase: true,
+		unique: true,
+		required: true,
+	},
+	credentials: {
+		salt: {
+			type: String,
+			required: true
+		},
+		password_hash: {
+			type: String,
+			required: true
+		}
+	},
+	currencies: currenciesSchema.obj
+})
 
 /**
  * Model da collection people, responsável por armazenar as informações dos
  * usuários
  */
-export = mongoose.model<Person>('Person', PersonSchema)
+export default mongoose.model<Person>('Person', PersonSchema)
