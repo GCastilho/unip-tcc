7a931735800fb55fa6040ce3a940106353ebe389
GCastilho <castilho.gabriel14@gmail.com>
2020-03-09 01:54:15 -0300

    Adicionado teste para user
    -- Adicionado teste para o entrypoint da user
    -- O request de wirhdraw será feito usando um POST em transactions
    -- O endpoint da transactions agora fica sob a user

--- tests/httpApi/v1.test.ts
+++ tests/httpApi/v1.test.ts
@@ -26,20 +26,20 @@ describe('Testing version 1 of HTTP API', () => {
 			entries: [
 				{
 					path: 'currencies',
-					description: 'Detailed information about the supported currencies of the system',
-					methods: [ 'GET' ],
-					auth: false
-				},
-				{
-					path: 'transaction',
-					description: 'Informations about a transaction',
-					methods: [ 'GET' ],
+					description: 'Request informations about the currencies supported by the API',
+					request: [{
+						method: 'GET',
+						returns: 'Detailed information about the supported currencies of the API'
+					}],
 					auth: false
 				},
 				{
 					path: 'user',
 					description: 'Entrypoint for requests specific to a user',
-					methods: [ 'GET' ],
+					request: [{
+						method: 'GET',
+						returns: 'Informations about the entrypoint'
+					}],
 					auth: false
 				}
 			]
@@ -59,31 +59,98 @@ describe('Testing version 1 of HTTP API', () => {
 		})
 	})
 
-	describe('/transaction', () => {
-		it('Should return information about the subpath')
-
-		describe('/:opid', () => {
-			it('Should return Not Authorized if invalid or missing sessionId')
-
-			it('Should return Not Authorized if transaction is from a different user')
-
-			it('Should return informations about the transaction')
-		})
-	})
-
 	describe('/user', () => {
-		it('Should return information about the subpath')
+		it('Should return information about the subpath', async () => {
+			const { body } = await request(app).get('/v1/currencies').set(apiConfig).send()
+				.expect('Content-Type', /json/)
+				.expect(200)
+			expect(body).to.be.an('object').that.equals({
+				description: 'Entrypoint for requests specific to a user',
+				entries: [
+					{
+						path: 'info',
+						description: 'Request informations about the user',
+						auth: true,
+						requests: [{
+							method: 'GET',
+							returns: 'Informations about the user'
+						}]
+					},
+					{
+						path: 'accounts',
+						description: 'Request a list of accounts of the user',
+						auth: true,
+						requests: [{
+							method: 'GET',
+							returns: 'List of accounts of all currencies that the user has',
+						}]
+					},
+					{
+						path: 'balances',
+						description: 'Request the balances for all the currencies',
+						auth: true,
+						requests: [{
+							method: 'GET',
+							returns: 'List of balances of all currencies',
+						}]
+					},
+					{
+						path: 'transactions',
+						description: 'Fetch, send and update transactions',
+						auth: true,
+						requests: [
+							{
+								method: 'GET',
+								returns: 'List of transactions from the user',
+								parametres: [
+									{
+										type: 'query',
+										description: '',
+										value: 'numeric',
+										name: 'from'
+									},
+									{
+										type: 'query',
+										description: '',
+										value: 'numeric',
+										name: 'to'
+									}
+								]
+							},
+							{
+								method: 'GET',
+								returns: 'Informations about specific transaction',
+								parametres: [
+									{
+										type: 'path',
+										description: 'The opid of the transaction to request data from',
+										value: 'string',
+										name: 'opid'
+									}
+								]
+							},
+							{
+								method: 'POST',
+								description: 'Submit new transaction',
+								returns: 'opid of the submitted transaction',
+								parametres: [
+									{
+										type: 'body',
+										description: 'Instructions to execute a withdraw of a currency',
+										value: {}
+									}
+								]
+							}
+						],
+					},
+				]
+			})
+		})
 
 		describe('/info', () => {
 			it('Should return Not Authorized if invalid or missing sessionId')
 
 			it('Should return user information')
-		})
-
-		describe('/balances', () => {
-			it('Should return Not Authorized if invalid or missing sessionId')
-
-			it('should return a balances object from the user')
 		})
 
 		describe('/accounts', () => {
@@ -92,12 +159,32 @@ describe('Testing version 1 of HTTP API', () => {
 			it('should return a accounts object from the user')
 		})
 
-		describe('/withdraw', () => {
+		describe('/balances', () => {
 			it('Should return Not Authorized if invalid or missing sessionId')
 
-			it('Should return Bad Request if the request is malformed')
+			it('should return a balances object from the user')
+		})
 
-			it('Should execute a withdraw for a given currency from the user')
+		describe('/transactions', () => {
+			it('Should return Not Authorized if invalid or missing sessionId')
+
+			it('Should return a list of transactions of the user')
+
+			describe('/:opid', () => {
+				it('Should return Not Authorized if invalid or missing sessionId')
+
+				it('Should return Not Authorized if transaction is from a different user')
+
+				it('Should return informations about the transaction')
+			})
+
+			describe('withdraw', () => {
+				it('Should return Not Authorized if invalid or missing sessionId')
+
+				it('Should return Bad Request if the request is malformed')
+
+				it('Should execute a withdraw for a given currency from the user')
+			})
 		})
 	})
 })
