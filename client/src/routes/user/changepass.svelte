059a7b043fa8a61c78db5f05fbab40b557f3059c
TTOExtreme <camarottol@hotmail.com>
2020-03-31 22:54:46 -0300

    Pagina de alteração de senha e Mudanças de Layout (#50)
    -- Adicionado uma página para atualização de senha do usuário
    -- Adicionado menu dropdown no header para o acesso do menu de alteração de senha
    -- Adicionado ícones em svg para alguns botões
    -- Adicionado testes de alteração de senha nos testes de login-cadastro
    -- Renomeado testes de requests de login-cadastro para se adequar ao fato de que ele não mais testa apenas o login e o cadastro
    
    Co-authored-by: TTOExtreme <TTOExtreme>
    Co-authored-by: GCastilho <castilho.gabriel14@gmail.com>

---
+++ client/src/routes/user/changepass.svelte
@@ -0,0 +1,90 @@
+<script>
+	import { onMount } from "svelte"
+	import { goto } from "@sapper/app"
+	import axios from "../../utils/axios.js"
+	import * as auth from "../../stores/auth.js"
+	import FancyInput from "../../components/FancyInput.svelte"
+	import FancyButton from "../../components/FancyButton.svelte"
+	import FormErrorMessage from "../../components/FormErrorMessage.svelte"
+
+	let errorMessage = undefined
+
+	onMount(() => {
+		// Redireciona para home caso não esteja autenticado
+		if (!$auth) goto('/')
+	})
+
+	async function handleSubmit(event) {
+		const oldPassword = event.target.password_old.value
+		const newPassword = event.target.password_new.value
+		const passwordconfirm = event.target.password_confirm.value
+
+		if (newPassword === passwordconfirm) {
+			try {
+				const { token } = await axios.post(
+					window.location,
+					{
+						oldPassword,
+						newPassword
+					}
+				)
+				/**
+				 * @todo Adicionar handlers para os erros vindos do sistema
+				 * de autenticação do websocket
+				 */
+				//await auth.authenticate(token)
+
+				/** Redireciona o usuário para a home */
+				goto("/")
+			} catch (err) {
+				if (err.response.status === 401) {
+					switch(err.response.data.error) {
+						case('NotLoggedIn'):
+							errorMessage = 'Authentication error'
+							break
+						case('NotAuthorized'):
+							errorMessage = 'Fail to verify old password'
+							break
+						default:
+							errorMessage = `Unknown error response: ${err.response.error}`
+					}
+				} else {
+					errorMessage = err.response.statusText
+				}
+			}
+		} else {
+			errorMessage = 'Passwords are different'
+		}
+	}
+</script>
+
+<style>
+	h1 {
+		text-align: center
+	}
+
+	form {
+		width: 350px;
+		height: 100%;
+		border: 1px solid lightgray;
+		margin-top: 1.5em;
+		margin-left: auto;
+		margin-right: auto;
+		padding: 20px;
+		border-radius: 6px;
+		box-shadow: 0px 5px 50px 0px rgba(18, 89, 93, 0.15);
+		line-height: 50px;
+	}
+</style>
+
+<h1>Alterar Senha</h1>
+<form on:submit|preventDefault={handleSubmit}>
+	{#if errorMessage}
+		<FormErrorMessage>{errorMessage}</FormErrorMessage>
+	{/if}
+	<FancyInput id="password_old" type="password" autocomplete="current-password">Old Password</FancyInput>
+	<FancyInput id="password_new" type="password" autocomplete="new-password">New Password</FancyInput>
+	<FancyInput id="password_confirm" type="password" autocomplete="new-password">Confirm Password</FancyInput>
+
+	<FancyButton type="submit">Update Password</FancyButton>
+</form>
