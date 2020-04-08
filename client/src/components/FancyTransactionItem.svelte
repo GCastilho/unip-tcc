a172e26667e3ca96b5bbce45189e923829124264
TTOExtreme <TTOExtreme>
2020-04-07 23:13:11 -0300

    WIP Tela de Transactions
    -- WIP Filtro de transações
    -- WIP Request da lista pela API
    -- NTI Request para um subdomínio
    -- NTI Salvar a lista na Store e verificar somente a ultima para checar se tem atualizado

---
+++ client/src/components/FancyTransactionItem.svelte
@@ -0,0 +1,20 @@
+<script>
+	export let transactionItem
+
+</script>
+
+<style>
+
+</style>
+<tr>
+	<td>{transactionItem['opid']}</td>
+	<td>{transactionItem['status']}</td>
+	<td>{transactionItem['currency']}</td>
+	<td>{transactionItem['txid']}</td>
+	<td>{transactionItem['account']}</td>
+	<td>{transactionItem['amount']}</td>
+	<td>{transactionItem['type']}</td>
+	<td>{transactionItem['confirmations']}</td>
+	<td>{transactionItem['timestamp']}</td>
+</tr>
+
