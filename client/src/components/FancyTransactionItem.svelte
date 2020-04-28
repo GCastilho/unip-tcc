f905dc9d7d1c28a571e1138dff53cb725d5d40ee
TTOExtreme <ttoextreme@gmai.com>
2020-04-28 17:14:49 -0300

    Error Handler na recepção de dados da api
    -- Add Informativo de erro na comunicação com a api
    -- Retirado dados do CorewDraw dos icones
    -- WIP layout dos itens da lista

--- client/src/components/FancyTransactionItem.svelte
+++ client/src/components/FancyTransactionItem.svelte
@@ -1,5 +1,6 @@
 <script>
 	export let transactionItem
+	let show = false;
 </script>
 
 <style>
@@ -11,15 +12,39 @@
 	tr{
 		border-top: 1px solid #80808080;
 	}
+	img{
+		height: 15px;
+	}
+	.noborder tr, .noborder td{
+		border: none;
+	}
+	.subinfo{
+		color: #606060;
+	}
 </style>
-<tr>
-	<td>{transactionItem['opid']}</td>
-	<td>{transactionItem['status']}</td>
-	<td>{transactionItem['currency']}</td>
-	<td>{transactionItem['txid']}</td>
-	<td>{transactionItem['account']}</td>
+<tr on:click={() => {show = !show; console.log(show)}}>
+	<td style="border-right:none"><img src="./assets/open-icon.svg" alt="open"></td>
+	
+	<td>
+		<table class="{'noborder'}">
+			<tr>
+				<td>{transactionItem['type']}</td>
+			</tr><tr>
+				<td class="{'subinfo'}">{transactionItem['status']}</td>
+			</tr>
+		</table>
+	</td>
 	<td>{transactionItem['amount']}</td>
-	<td>{transactionItem['type']}</td>
-	<td>{transactionItem['confirmations']}</td>
+
+	<td>
+		<table class="{'noborder'}">
+			<tr>
+				<td>{transactionItem['txid']}</td>
+			</tr><tr>
+				<td class="{'subinfo'}">{transactionItem['account']}</td>
+			</tr>
+		</table>
+	</td>
 	<td style="border-right:none">{transactionItem['timestamp']}</td>
+
 </tr>
