242540d4a450974f078bf0cb974d32c3a54378da
TTOExtreme <ttoextreme@gmai.com>
2020-05-05 19:55:12 -0300

    Alterado layout
    -- Alterado carregamento de mais dados ao chegar no final (ultimos 100px da pagina)
    -- Alterado distancia entre objetos (melhorias para modo Mobile)
    -- Alterado filtros da tabela (retirado colunas adicionais)
    -- Adicionado Dropdown para mais detalhes das transactions

--- client/src/components/FancyTransactionItem.svelte
+++ client/src/components/FancyTransactionItem.svelte
@@ -10,6 +10,10 @@
 		padding-left: 10px;
 	}
 	tr{
+		border: 2px solid #80808080;
+		border-bottom: none;
+	}
+	.hide_segment{
 		border-top: 1px solid #80808080;
 	}
 	img{
@@ -21,10 +25,16 @@
 	.subinfo{
 		color: #606060;
 	}
+	table{
+		width: 100%;
+	}
 </style>
 <tr on:click={() => {show = !show; console.log(show)}}>
-	<td style="border-right:none"><img src="./assets/open-icon.svg" alt="open"></td>
-	
+	{#if show}
+		<td style="border-right:none"><img src="./assets/open-icon.svg" alt="open" title="Open More Info"></td>
+	{:else}
+		<td style="border-right:none"><img src="./assets/close-icon.svg" alt="close" title="Close More Info"></td>
+	{/if}
 	<td>
 		<table class="{'noborder'}">
 			<tr>
@@ -40,11 +50,19 @@
 		<table class="{'noborder'}">
 			<tr>
 				<td>{transactionItem['txid']}</td>
-			</tr><tr>
+			</tr>
+			<tr>
 				<td class="{'subinfo'}">{transactionItem['account']}</td>
 			</tr>
 		</table>
 	</td>
 	<td style="border-right:none">{transactionItem['timestamp']}</td>
-
 </tr>
+{#if show}
+<tr class="hide_segment">
+	<td style="border:none"></td>
+	<td colspan="2">OpId: {transactionItem['opid']}</td>
+	<td>Currency: {transactionItem['currency']}</td>
+	<td style="border-right:none">Confirmations: {transactionItem['confirmations']}</td>
+</tr>
+{/if}
