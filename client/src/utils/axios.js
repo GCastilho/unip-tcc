c844b72b98efe9597dfdda88009e64a6e1810e64
GCastilho <castilho.gabriel14@gmail.com>
2020-06-23 21:05:14 -0300

    Consertado porta do axios
    -- O módulo do axios agora manda para a porta 3001 se em dev e para a 3000 se não

--- client/src/utils/axios.js
+++ client/src/utils/axios.js
@@ -1,6 +1,8 @@
 import axios from 'axios'
 
+const port = process.env.NODE_ENV === 'development' ? 3001 : 3000
+
 export default axios.create({
-	baseURL: 'http://api.localhost:3001',
+	baseURL: `http://api.localhost:${port}`,
 	withCredentials: true
 })
