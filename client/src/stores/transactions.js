cdc12e1a3894e432eccec847928167da87df7dac
GCastilho <castilho.gabriel14@gmail.com>
2020-08-11 15:18:25 -0300

    Refatorado store de transactions
    -- Refatorado os métodos da store de transactions sem afetar o funcionamento

--- client/src/stores/transactions.js
+++ client/src/stores/transactions.js
@@ -1,4 +1,4 @@
-import { writable } from 'svelte/store'
+import { writable, get } from 'svelte/store'
 import axios from '../utils/axios'
 import * as auth from './auth'
 import { addSocketListener } from '../utils/websocket'
@@ -11,17 +11,14 @@ const { subscribe, set, update } = writable([])
 export { subscribe }
 
 /** Faz um request de um array de transações e seta na store */
-export const loadTx = async () => {
-	const unsubscribeAuth = auth.subscribe(async auth => {
-		if (!auth) return
-		try {
-			const tx = await axios.get('/v1/user/transactions')
-			set(tx.data)
-		} catch(err) {
-			console.log(err)
-		}
-	})
-	unsubscribeAuth()
+export async function loadTx() {
+	if (!get(auth)) return
+	try {
+		const tx = await axios.get('/v1/user/transactions')
+		set(tx.data)
+	} catch(err) {
+		console.error(err)
+	}
 }
 
 /**
@@ -29,9 +26,9 @@ export const loadTx = async () => {
  * e concatena na store existente
  * @param {number} skip numero de transações que sera puladas
  */
-export const reloadTx = async (skip) => {
+export async function reloadTx(skip) {
 	try {
-		const tx = await axios.get('/v1/user/transactions', {params: {skip}})
+		const tx = await axios.get('/v1/user/transactions', { params: {skip} })
 		update(value => value.concat(tx.data))
 	} catch(err) {
 		console.log(err)
@@ -40,36 +37,33 @@ export const reloadTx = async (skip) => {
 
 /** Atualiza a store ao receber uma nova transação */
 addSocketListener('new_transaction', async (currency, transaction) => {
-	update(tx => {
-		tx.unshift(transaction)
-		return tx
-	})
+	update(tx => [transaction, ...tx])
 })
 
 /** Atualiza uma transação 'receive' existente na store */
 addSocketListener('update_received_tx', async (currency, transaction) => {
-	console.log(transaction)
-	console.log('update_received_tx')
+	console.log('update_received_tx', transaction)
 	update(tx => {
 		const index = tx.findIndex(tx =>
 			tx.opid === transaction.opid ||
 			tx.txid === transaction.txid
 		)
-		if (index >= 0) tx[index] = {...tx[index], ...transaction}
+		if (index >= 0)
+			tx[index] = {...tx[index], ...transaction}
 		return tx
 	})
 })
 
 /** Atualiza uma transação 'send' existente na store */
 addSocketListener('update_sent_tx', async (currency, transaction) => {
-	console.log(transaction)
-	console.log('update_sent_tx')
+	console.log('update_sent_tx', transaction)
 	update(tx => {
 		const index = tx.findIndex(tx =>
 			tx.opid === transaction.opid ||
 			tx.txid === transaction.txid
 		)
-		if (index >= 0) tx[index] = {...tx[index], ...transaction}
+		if (index >= 0)
+			tx[index] = {...tx[index], ...transaction}
 		return tx
 	})
 })
