2abd47d78aa2d49e43d02db04ef93aaf4e0f9fdd
GCastilho <castilho.gabriel14@gmail.com>
2020-09-05 15:52:39 -0300

    Refatorado store de transactions
    -- Refatorado store de balances, ela agora não atualiza o saldo dela própria, mas permite uma atualização dele por outros módulos através de um método 'updateBalances'
    -- Refatorado a store de transactions, ela agora atualiza o saldo da store de balances quando tem uma transação ou atualização de transação
    -- Adicionado um método 'withdraw' na store de transactions para ser o método pelo qual o sistema faz uma nova transação. Esse método envia o request para a API e adiciona a transação ao array de transactions

--- client/src/stores/transactions.js
+++ client/src/stores/transactions.js
@@ -1,26 +1,26 @@
 import { writable } from 'svelte/store'
 import axios from '../utils/axios'
 import { addSocketListener } from '../utils/websocket'
+import { updateBalances } from './balances'
 import * as auth from './auth'
 
 const { subscribe, update, set } = writable([])
+
+/** Exporta o subscribe para esse módulo ser uma store */
+export { subscribe }
+
+/** Flag que indica se a função de fetch está sendo executada */
+let inSync = false
+
+/** Indica se o cliente está sincronizado com as transações do servidor */
+let fullySync = false
 
 /** Quantidade de transações presentes na store */
 let storeLength = 0
 subscribe(v => storeLength = v.length)
 
-/** Indica se o cliente está sincronizado com as transações do servidor */
-let fullySync = false
-
-/** Flag que indica se a função de fetch está sendo executada */
-let inSync = false
-
-/** Exporta o subscribe para esse módulo ser uma store */
-export { subscribe }
-
 /**
- * Busca por mais 10 transações caso a quantidade pedida não esteja na store
- * @param {number} [skip] O número de transações que devem ser puladas
+ * Busca por mais 10 transações caso a não esteja sincronizada com o servidor
  */
 export async function fetch() {
 	if (fullySync || inSync) return
@@ -62,36 +62,73 @@ auth.subscribe(v => {
 	else set([])
 })
 
+/**
+ * Realiza um request de saque e atualiza a store de transactions
+ * @param {string} currency A currency que será sacada
+ * @param {string} destination O endereço de destino que as moedas devem ser enviadas
+ * @param {number} amount A quantidade dessa currency que será sacada
+ */
+export async function withdraw(currency, destination, amount) {
+	/** @type {{data: string}} O opid do request de saque */
+	const { data } = await axios.post('/v1/user/transactions', {
+		currency,
+		destination,
+		amount
+	})
+
+	update(txs => ([
+		{
+			opid: data,
+			currency,
+			account: destination,
+			amount,
+			fee: 0 // Fix
+		},
+		...txs
+	]))
+
+	updateBalances(currency, -amount, amount)
+}
+
 /** Atualiza a store ao receber uma nova transação */
-addSocketListener('new_transaction', async (currency, transaction) => {
+addSocketListener('new_transaction', (currency, transaction) => {
 	console.log('new_transaction', transaction)
-	update(tx => [transaction, ...tx])
+	update(txs => [transaction, ...txs])
+	if (transaction.status == 'confirmed') {
+		updateBalances(currency, transaction.amount, 0)
+	} else {
+		updateBalances(currency, 0, transaction.amount)
+	}
 })
 
 /** Atualiza uma transação 'receive' existente na store */
-addSocketListener('update_received_tx', async (currency, transaction) => {
-	console.log('update_received_tx', transaction)
+addSocketListener('update_received_tx', async (currency, txUpdate) => {
+	console.log('update_received_tx', txUpdate)
 	update(tx => {
 		const index = tx.findIndex(tx =>
-			tx.opid === transaction.opid ||
-			tx.txid === transaction.txid
+			tx.opid === txUpdate.opid ||
+			tx.txid === txUpdate.txid
 		)
 		if (index >= 0)
-			tx[index] = {...tx[index], ...transaction}
+			tx[index] = {...tx[index], ...txUpdate}
+		if (txUpdate.status == 'confirmed')
+			updateBalances(currency, txUpdate.amount, -txUpdate.amount)
 		return tx
 	})
 })
 
 /** Atualiza uma transação 'send' existente na store */
-addSocketListener('update_sent_tx', async (currency, transaction) => {
-	console.log('update_sent_tx', transaction)
+addSocketListener('update_sent_tx', async (currency, txUpdate) => {
+	console.log('update_sent_tx', txUpdate)
 	update(tx => {
 		const index = tx.findIndex(tx =>
-			tx.opid === transaction.opid ||
-			tx.txid === transaction.txid
+			tx.opid === txUpdate.opid ||
+			tx.txid === txUpdate.txid
 		)
 		if (index >= 0)
-			tx[index] = {...tx[index], ...transaction}
+			tx[index] = {...tx[index], ...txUpdate}
+		if (txUpdate.status == 'confirmed')
+			updateBalances(currency, 0, -txUpdate.amount)
 		return tx
 	})
 })
