cdd21ef41c8c4ec6c764ac7bd3903a85ab83658d
GCastilho <castilho.gabriel14@gmail.com>
2020-10-01 22:46:11 -0300

    Movido connection para o index da Currency
    -- O método connection foi motivo de um arquivo próprio dentor da methods para dentro do index da Currency

--- src/currencyApi/currency/index.ts
+++ src/currencyApi/currency/index.ts
@@ -1,10 +1,11 @@
+import socketIO from 'socket.io'
+import ss from 'socket.io-stream'
 import { ObjectId } from 'mongodb'
 import { EventEmitter } from 'events'
 import initListeners from './listeners'
 import Person from '../../db/models/person'
 import ChecklistDoc from '../../db/models/checklist'
 import TransactionDoc, { Transaction } from '../../db/models/transaction'
-import * as methods from './methods'
 import * as UserApi from '../../userApi'
 import type TypedEmitter from 'typed-emitter'
 import type User from '../../userApi/user'
@@ -81,9 +82,6 @@ export default class Currency {
 
 	/** EventEmmiter para eventos públicos */
 	public events = new EventEmitter() as TypedEmitter<PublicEvents>
-
-	/** Handler da conexão com o módulo externo */
-	public connection = methods.connection
 
 	public async createAccount(userId: ObjectId): Promise<string> {
 		const account = await this.emit('create_new_account')
@@ -180,6 +178,72 @@ export default class Currency {
 			this.loop()
 				.catch(err => console.error('Error on loop method for', this.name, err))
 				.finally(() => this.looping = false)
+		})
+	}
+
+	/** Handler da conexão com o módulo externo */
+	public connection(socket: socketIO.Socket) {
+		/*
+		* Essa função é executada (pela currencyApi) quando o socket se conecta
+		*/
+		this.isOnline = true
+		this._events.emit('connected')
+
+		/**
+		 * Emite um 'disconnected' no event emitter interno para informar aos outros
+		 * métodos que o socket se desconectou
+		 */
+		socket.on('disconnect', () => {
+			console.log(`Disconnected from the '${this.name}' module`)
+			this.isOnline = false
+			this._events.removeAllListeners('emit')
+			this._events.emit('disconnected')
+		})
+
+		for (const event of this._events.eventNames()) {
+		// Pula os eventos que não são do módulo externo
+			switch (event) {
+				case('connected'):
+				case('disconnected'):
+				case('emit'):
+					continue
+			}
+			socket.on(event, (...args) => {
+			// @ts-expect-error A tipagem desses eventos é feita separadamente
+				this._events.emit(event, ...args)
+			})
+		}
+
+		/**
+		 * Retorna uma stream de strings de todas as accounts dos clientes, uma
+		 * account por chunk
+		 */
+		ss(socket).on('get_account_list', (stream: NodeJS.WritableStream) => {
+			const person = Person.find({}, {
+				[`currencies.${this.name}`]: 1
+			}).lean().cursor()
+
+			person.on('data', ({ currencies }) => {
+				if (Object.keys(currencies).length === 0) return
+				currencies[this.name].accounts.forEach((account: string) => {
+					stream.write(account)
+				})
+			})
+
+			person.on('end', () => stream.end())
+		})
+
+		/**
+		 * Ouve por eventos vindos do método 'module' e os retransmite ao socket
+		 * para serem enviados ao módulo externo
+		 */
+		this._events.on('emit', (event, args, callback) => {
+			console.log('event', event)
+			if (this.isOnline) {
+				socket.emit(event, ...args, callback)
+			} else {
+				callback('SocketDisconnected')
+			}
 		})
 	}
 
