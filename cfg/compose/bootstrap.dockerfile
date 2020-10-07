eb21582cb9a8be6fef45dcfd8aa7f7444dd802a5
gabr1gus <gabrielgustavorodrigues@gmail.com>
2020-10-07 00:42:08 -0300

    bootstrap funcionando agora
    -- corrigido erros do bootstrap.sh
    -- removido arquivos desnecessarios
    -- removido linhas de codigo desnecessarias

--- cfg/compose/bootstrap.dockerfile
+++ cfg/compose/bootstrap.dockerfile
@@ -4,4 +4,4 @@ COPY ./bootstrap.sh /
 
 RUN chmod +x /bootstrap.sh
 
-ENTRYPOINT ["/bootstrap.sh"]
+CMD ["/bootstrap.sh"]
--- cfg/compose/bootstrap.sh
+++ cfg/compose/bootstrap.sh
@@ -12,34 +12,43 @@ shardnames=(
 router="mongo-router:27017"
 
 configLoop=1
-while [configLoop -eq 1]; do
+while [ $configLoop -eq 1 ]; do
 	sleep 5
-	for host of ${hostnames[@]}; do
+	for host in ${hostnames[@]}; do
 		configLoop=0
-		/usr/bin/mongo $host --eval '{ ping: 1 }'
+		echo -n "connecting to config server ${host}... "
+		/usr/bin/mongo $host --eval '{ ping: 1 }' > /dev/null
 		if [ $? -eq 1 ]; then
+			echo "Fail"
 			configLoop=1
+		else
+			echo "Success"
 		fi
 	done
 done
 
-/usr/bin/mongo hostnames[0] --eval 'rs.initiate({ "_id":"repset", "members":[ {"_id":0,"host":"config-01:27019"}, {"_id":1,"host":"config-02:27019"}, {"_id":2,"host":"config-03:27019"} ]})'
+/usr/bin/mongo ${hostnames[0]} --eval 'rs.initiate({ "_id":"repset", "members":[ {"_id":0,"host":"config-01:27019"}, {"_id":1,"host":"config-02:27019"}, {"_id":2,"host":"config-03:27019"} ]})'
 
 shardLoop=1
-while [$shardLoop -eq 1]; do
+while [ $shardLoop -eq 1 ]; do
 	sleep 5
-	for shard of ${shardnames[@]}; do
-		configLoop=0
-		/usr/bin/mongo $shard --eval '{ ping: 1 }'
+	for shard in ${shardnames[@]}; do
+		shardLoop=0
+		echo -n "connecting to shard server ${shard}... "
+		/usr/bin/mongo $shard --eval '{ ping: 1 }' > /dev/null
 		if [ $? -eq 1 ]; then
-			configLoop=1
+			echo "Fail"
+			shardLoop=1
+		else
+			echo "Success"
 		fi
 	done
 done
 
-/usr/bin/mongo shardnames[0] --eval 'rs.initiate({ "_id":"repset", "members":[ {"_id":0,"host":"shard1a:27018"}, {"_id":1,"host":"shard1b:27018"}]})'
+/usr/bin/mongo $shardnames --eval 'rs.initiate({ "_id":"shard01", "members":[ {"_id":0,"host":"shard1a:27018"}, {"_id":1,"host":"shard1b:27018"}]})'
 
-for shard of ${shardnames[@]}; do
-	/usr/bin/mongo router --eval `\'sh.addShard(shard01/${$shard})\'`
+for shard in ${shardnames[@]}; do
+	/usr/bin/mongo $router --eval \'"sh.addShard(shard01/$shard)"\' > /dev/null
 done
 
+echo "done"
