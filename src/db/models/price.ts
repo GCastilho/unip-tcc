0cec9e46b35b14c15478c387c6523ae7dd58e32b
gabr1gus <gabrielgustavorodrigues@gmail.com>
2020-10-19 20:47:34 -0300

    collection pricehistory
    -- pre save para garantir a ordenaçao do array currencies
    -- validate para garantir o numero e tipo dos itens do array currencies

---
+++ src/db/models/price.ts
@@ -0,0 +1,47 @@
+import { Document, Schema } from '../mongoose'
+import { currencyNames, SuportedCurrencies } from '../../libs/currencies'
+import { mongoose } from '../../../external_modules/common/db/mongoose'
+
+/** Objeto de atualização de preço */
+interface PriceUpdate extends Document {
+	/** Novo preço desse par */
+	price: number
+	/** O preço que está sendo modificado */
+	type: 'buy'|'sell'
+	/** As currencies que fazem parte desse par */
+	currencies: [SuportedCurrencies, SuportedCurrencies]
+}
+
+const priceUpdateSchema = new Schema({
+	price: {
+		type: Number,
+		required: true
+	},
+	type: {
+		type: String,
+		enum: ['buy', 'sell'],
+		required: true
+	},
+	currencies: {
+		type: [String],
+		required: true,
+		validate: {
+			validator: (currencies: SuportedCurrencies[]) => currencies.length == 2
+				&& currencies.every(item => currencyNames.includes(item))
+				&& currencies[0] != currencies[1],
+			message: 'currencies lenght must be two and currency type must be SuportedCurrencies',
+		}
+	}
+})
+
+priceUpdateSchema.pre('save', function(this:PriceUpdate) {
+	this.currencies = this.currencies.sort((a, b) => {
+		return a > b ? 1 : a < b ? -1 : 0
+	})
+})
+
+const Price = mongoose.model('price', priceUpdateSchema, 'pricehistory')
+
+export default Price
+
+
