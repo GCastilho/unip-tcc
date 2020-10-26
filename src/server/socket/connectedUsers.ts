d14b6f9195627c8451fc6b4c780db61008ea8347
GCastilho <castilho.gabriel14@gmail.com>
2020-10-26 15:02:58 -0300

    Refatorado servidor do websocket
    -- Refatorado servidor do socketIO para agora receber uma instancia do socket.io ao inves de um servidor HTTP
    -- O get da connectedUsers agora retorna uma classe (que estende um set) com um metodo emit que emite um evento para todos os sockets daquele set
    -- Os emitters do weboskcet agora ficam no index

--- src/server/socket/connectedUsers.ts
+++ src/server/socket/connectedUsers.ts
@@ -1,12 +1,19 @@
 import type { Socket } from 'socket.io'
 import type { PersonDoc } from '../../db/models/person'
 
+/** Um set de sockets com um método que emite um evento em todos eles */
+class SocketSet extends Set<Socket> {
+	public emit(event: string, ...args: any[]) {
+		this.forEach(socket => socket.emit(event, ...args))
+	}
+}
+
 /**
  * Map de usuários conectados no sistema, a chave é a string do userId do
  * usuário e o valor é um set de todos os socket desse usuário que estão
  * conectados
  */
-const connectedUsers = new Map<PersonDoc['id'], Set<Socket>>()
+const connectedUsers = new Map<PersonDoc['id'], SocketSet>()
 
 /**
  * Adiciona o socket de um usuário autenticado ao map de usuários conectados
@@ -19,18 +26,18 @@ export function add(socket: Socket): void {
 
 	const set = connectedUsers.get(socket.userId)
 	if (set) set.add(socket)
-	else connectedUsers.set(socket.userId, new Set([socket]))
+	else connectedUsers.set(socket.userId, new SocketSet([socket]))
 }
 
 /**
  * Retorna o set de sockets de um usuário conectado
  * @param userId O userId do usuário
  *
- * @returns Set<Socket> - caso o usuário esteja conectado
+ * @returns SocketSet - caso o usuário esteja conectado
  * @returns undefined - caso o usuário NÃO esteja conectado
  */
-export function get(userId: PersonDoc['id']): Set<Socket>|undefined {
-	return connectedUsers.get(userId)
+export function get(userId: PersonDoc['_id']): SocketSet|undefined {
+	return connectedUsers.get(userId.toHexString())
 }
 
 /**
