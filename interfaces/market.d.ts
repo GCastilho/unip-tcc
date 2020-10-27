cdd94058e8eed60a4d4992e68d2198a74697cd93
GCastilho <castilho.gabriel14@gmail.com>
2020-10-27 13:13:20 -0300

    Atualizado interfaces do market-comunication

--- interfaces/market.d.ts
+++ interfaces/market.d.ts
@@ -1,16 +1,6 @@
 import type { SuportedCurrencies } from '../src/libs/currencies'
 
-/** Objeto de atualização de preço */
-export interface PriceUpdate {
-	/** Novo preço desse par */
-	price: number
-	/** O preço que está sendo modificado */
-	type: 'buy'|'sell'
-	/** As currencies que fazem parte desse par */
-	currencies: [SuportedCurrencies, SuportedCurrencies]
-}
-
-/** Objeto de uma ordem de trade da MarketApi */
+/** Interface de uma ordem de trade da MarketApi */
 export interface MarketOrder {
 	owning: {
 		currency: SuportedCurrencies
@@ -20,4 +10,32 @@ export interface MarketOrder {
 		currency: SuportedCurrencies
 		amount: number
 	}
+}
+
+/** Interface de um request das margens de preço de um mercado */
+export interface PriceRequest {
+	/** Preço da maior ordem de compra */
+	buyPrice: number
+	/** Preço da menor ordem de venda */
+	sellPrice: number
+	/** As currencies que fazem parte desse par */
+	currencies: [SuportedCurrencies, SuportedCurrencies]
+}
+
+/** Interface de atualização de preço */
+export interface PriceUpdate {
+	/** Novo preço desse par */
+	price: number
+	/** O preço que está sendo modificado */
+	type: 'buy'|'sell'
+	/** As currencies que fazem parte desse par */
+	currencies: [SuportedCurrencies, SuportedCurrencies]
+}
+
+/** Interface do objeto de informação do depth do mercado */
+export interface MarketDepth {
+	price: number
+	volume: number
+	type: 'buy'|'sell'
+	currencies: [SuportedCurrencies, SuportedCurrencies]
 }
