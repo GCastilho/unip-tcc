82ef80cfd4faab1efc6f6d059bc83dae53c48d37
GCastilho <castilho.gabriel14@gmail.com>
2020-12-01 20:33:22 -0300

    Implementado função de logout
    -- Implementado uma rota DELETE em /login, que recebe os requests de logout e roteia-os para a API
    -- Implementado uma função de logout no componente Nav, que manda o request de logout para a rota acima e chama o método deauthenticate do websocket
    -- A store de autenticação infelizmente ainda é necessária, pois o sapper não deixa módulos acessarem a store de autenticação, então ela foi refatorada para ser uma store derivada da store de sessão, alterando seu estado conforme ela alterar
    -- Trocado store de autenticação para um TS e adicionado tipagem nas props

--- client/src/routes/login.ts
+++ client/src/routes/login.ts
@@ -19,3 +19,20 @@ export async function post(req: Request, res: Response) {
 		errorHandler(res, err)
 	}
 }
+
+export async function del(req: Request, res: Response) {
+	try {
+		await axios.delete('/v1/user/authentication', {
+			baseURL: mainServerIp,
+			headers: {
+				Authorization: req.cookies.sessionId
+			}
+		})
+
+		// Seta o cookie para expirar no passado, fazendo com o que browser o delete
+		res.cookie('sessionId', '', { httpOnly: true, expires: new Date(0) })
+		res.send({ message: 'Deauthenticated' })
+	} catch (err) {
+		errorHandler(res, err)
+	}
+}
