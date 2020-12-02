606b0d938791294ade11e6b5d806313fb02e7ced
GCastilho <castilho.gabriel14@gmail.com>
2020-12-01 21:58:34 -0300

    Refatorado store de balances
    -- Implementado uma rota do sapper para retornar o objeto de balances do usuário em /user/balances
    -- Refatorado a store de balances para usar typescript e ser fortemente tipada

---
+++ client/src/stores/balances.ts
@@ -0,0 +1,60 @@
+import axios from 'axios'
+import { writable } from 'svelte/store'
+import currencies from '../utils/currencies'
+import * as auth from './auth'
+import type { Balances } from '../routes/user/balances'
+
+/** Retorna um objeto com o type da store mas com os saldos zerados */
+function emptyStore(): Balances {
+	const store = {} as Balances
+	for (const currency in currencies) {
+		if (Object.prototype.hasOwnProperty.call(currencies, currency)) {
+			store[currency] = {
+				available: 0,
+				locked: 0
+			}
+		}
+	}
+	return store
+}
+
+const { subscribe, set, update } = writable<Balances>(emptyStore())
+
+/**
+ * Exporta a store para permitir modificação do saldo de fora
+ */
+export { subscribe, set }
+
+/**
+ * Ao autenticar com o socket atualiza o saldo com o valor do servidor
+ */
+auth.subscribe(async authenticated => {
+	if (authenticated) {
+		try {
+			const { data } = await axios.get<Balances>('/user/balances')
+			set(data)
+		} catch (err) {
+			console.error('Error while fetching balances:', err.response.statusText)
+		}
+	} else {
+		set(emptyStore())
+	}
+})
+
+/**
+ *
+ * @param currency A currency que o saldo será atualizado
+ * @param available Quanto o available deve ser incrementado
+ * @param locked Quanto o locked deverá ser incrementado
+ */
+export function updateBalances(
+	currency: keyof Balances,
+	available: number,
+	locked: number,
+) {
+	update(balances => {
+		balances[currency].available += available
+		balances[currency].locked += locked
+		return balances
+	})
+}
