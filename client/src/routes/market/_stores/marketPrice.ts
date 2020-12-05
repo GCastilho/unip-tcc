4731286c9c8af81772e25b3ca779dd639a66c0a0
GCastilho <castilho.gabriel14@gmail.com>
2020-12-04 21:55:30 -0300

    Adicionado rota do sapper para market price
    -- Adicionado uma rota do sapper em /market/price para retornar o marketPrice da API
    -- A store de marketPrice agora retorna os preços da rota do sapper

--- client/src/routes/market/_stores/marketPrice.ts
+++ client/src/routes/market/_stores/marketPrice.ts
@@ -1,22 +1,27 @@
+import axios from 'axios'
 import { writable } from 'svelte/store'
-import axios from '../../../utils/axios'
 import { addSocketListener } from '../../../utils/websocket'
-import type { PriceRequest, PriceUpdate } from '../../../../../interfaces/market'
+import type { PriceRequest } from '../price'
+import type { PriceUpdate } from '../../../../../interfaces/market'
 
-const { subscribe, update, set } = writable<PriceRequest>({})
+const { subscribe, update, set } = writable<PriceRequest>({
+	buyPrice: 0,
+	sellPrice: Infinity,
+	currencies: [] as unknown as PriceRequest['currencies']
+})
 
 /** Exporta o subscribe para essa variável se ruma store */
 export { subscribe }
 
 /** Pega os dados do grafico e popula a store */
-export async function fetch(currencies:string[]) {
+export async function fetch(currencies: PriceRequest['currencies']) {
 	try {
 		if (!currencies[0] || !currencies[1] || currencies[0] == currencies[1]) return
 		currencies.sort()
-		const { data } = await axios.get('/v1/market/price', {
+		const { data } = await axios.get<PriceRequest>('/market/price', {
 			params: { base: currencies[0], target: currencies[1] }
 		})
-		console.log(data)
+		console.log('market price fetch:', data)
 		set(data)
 	} catch (err) {
 		console.error('Error fetching prices', err)
@@ -24,8 +29,8 @@ export async function fetch(currencies:string[]) {
 }
 
 /** Atualiza o array da store ao receber o evento price_update */
-addSocketListener('price_update', (newPrice:PriceUpdate) => {
-	console.log(newPrice)
+addSocketListener('price_update', (newPrice: PriceUpdate) => {
+	console.log('price_update', newPrice)
 	update(price => {
 		if (newPrice.type == 'buy') {
 			price.buyPrice = newPrice.price
