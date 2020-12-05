a2f0d795b175fa9218f3ac5ed26450a35cdb934a
GCastilho <castilho.gabriel14@gmail.com>
2020-12-04 21:45:05 -0300

    Adicionado rota para o market depth
    -- Adicionado uma rota /market/depth no sapper para retornar o market depth da API
    -- A store de depth agora usa a nova rota

--- client/src/routes/market/_stores/depth.ts
+++ client/src/routes/market/_stores/depth.ts
@@ -1,7 +1,7 @@
+import axios from 'axios'
 import { writable } from 'svelte/store'
-import axios from '../../../utils/axios'
 import { addSocketListener } from '../../../utils/websocket'
-import type { MarketDepth } from '../../../../../interfaces/market'
+import type { MarketDepth } from '../depth'
 
 const { subscribe, update, set } = writable<MarketDepth[]>([])
 
@@ -13,10 +13,10 @@ export async function fetch(currencies:string[]) {
 	try {
 		if (!currencies[0] || !currencies[1] || currencies[0] == currencies[1]) return
 		currencies.sort()
-		const { data } = await axios.get('/v1/market/orderbook/depth', {
+		const { data } = await axios.get<MarketDepth[]>('/market/depth', {
 			params: { base: currencies[0], target: currencies[1] }
 		})
-		console.log(data)
+		console.log('depth fetch:', data)
 		set(data)
 	} catch (err) {
 		console.error('Error fetching orders', err)
@@ -24,7 +24,7 @@ export async function fetch(currencies:string[]) {
 }
 
 /** Atualiza o array da store ao receber o evento depth_update */
-addSocketListener('depth_update', (depth:MarketDepth) => {
+addSocketListener('depth_update', (depth: MarketDepth) => {
 	update(columns => {
 		const index = columns.findIndex(v => v.type === depth.type && v.price > depth.price)
 		columns.splice(index, 0, depth)
