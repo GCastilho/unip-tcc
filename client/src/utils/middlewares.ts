f6028da2b51f5a6391367907a0b38f1ba2644199
GCastilho <castilho.gabriel14@gmail.com>
2020-12-07 19:43:07 -0300

    Implementado uma função para facilitar os requests ao main server
    -- Implementado uma função que cria um middleware do express que irá servir como proxy para o main server

--- client/src/utils/middlewares.ts
+++ client/src/utils/middlewares.ts
@@ -1,5 +1,6 @@
-import type { Response } from 'express'
-import type { AxiosError } from 'axios'
+import axios from 'axios'
+import type { Request, Response, NextFunction } from 'express'
+import type { AxiosError, AxiosResponse } from 'axios'
 
 export const mainServerIp = process.env.MAIN_SERVER_IP || 'http://127.0.0.1:3001'
 
@@ -22,5 +23,48 @@ export function errorHandler(
 		res.status(500).send({
 			error: 'Internal Server Error'
 		})
+	}
+}
+
+/**
+ * Retorna um middleware do express para redirecionar requests ao main server
+ * @param method O método HTTP do request que será feito
+ * @param url A url do main server que deve ser requisitada
+ */
+export function apiRequest(
+	method: 'get'|'post'|'patch'|'delete',
+	url: string,
+) {
+	return async function<T>(req: Request, res: Response, next: NextFunction) {
+		/** Envia ao sapper todos os requests que não esperam JSON como resposta */
+		if (!req.headers.accept.includes('application/json')) return next()
+
+		const params = url.match(new RegExp(':.+?(?=/|$)', 'g'))
+		if (params) {
+			for (const param of params) {
+				url = url.replace(param, req.params[param.slice(1)])
+			}
+		}
+
+		const Axios = axios.create({
+			baseURL: mainServerIp,
+			params: req.query,
+			headers: {
+				Authorization: `${req.cookies.sessionId}`
+			}
+		})
+
+		try {
+			let response: AxiosResponse<T>
+			if (method == 'get' || method == 'delete') {
+				response = await Axios[method](url)
+			} else {
+				response = await Axios[method](url, req.body)
+			}
+
+			res.status(response.status).send(response.data)
+		} catch (err) {
+			errorHandler(res, err)
+		}
 	}
 }
