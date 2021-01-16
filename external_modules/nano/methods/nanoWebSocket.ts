a01263652e466418ffd68b09b578baf541691df2
GCastilho <castilho.gabriel14@gmail.com>
2021-01-16 00:12:42 -0300

    Refatorado nanoWebSocket
    -- Refatorado nanowebSocket
    
    Co-authored-by @gabr1gus

--- external_modules/nano/methods/nanoWebSocket.ts
+++ external_modules/nano/methods/nanoWebSocket.ts
@@ -1,28 +1,23 @@
 import WS from 'ws'
-import { Nano } from '../index'
-// eslint-disable-next-line
-const ReconnectingWebSocket = require('reconnecting-websocket')
+import ReconnectingWebSocket from 'reconnecting-websocket'
+import type { Nano } from '../index'
+import type { AxiosError } from 'axios'
 
 const nanoSocketUrl = process.env.NANO_SOCKET_URL || 'ws://[::1]:57000'
 
 export function nanoWebSocket(this: Nano) {
-	/**
-	 * Create a reconnecting WebSocket. In this example, we wait a maximum of
-	 * 2 seconds before retrying
-	 */
 	const ws = new ReconnectingWebSocket(nanoSocketUrl, [], {
 		WebSocket: WS,
 		connectionTimeout: 10000,
 		maxRetries: 100000,
-		maxReconnectionDelay: 2000,
-		minReconnectionDelay: 10 // if not set, initial connection will take a few seconds by default
+		maxReconnectionDelay: 2000, // Wait max of 2 seconds before retrying
 	})
 
 	/** Keep track if this is the first websocket connection */
 	let firstConnection = true
 
 	/** As soon as we connect, subscribe to block confirmations */
-	ws.onopen = () => {
+	ws.addEventListener('open', () => {
 		console.log('Websocket connection open')
 		firstConnection = false
 		this._events.emit('rpc_connected')
@@ -35,28 +30,23 @@ export function nanoWebSocket(this: Nano) {
 				//accounts: [array de contas extras que serão observadas]
 			}
 		}))
-	}
+	})
 
 	/**
 	 * The node sent us a message
-	 *
-	 * @todo Tratar os dados e enviar para o servidor e banco de dados
 	 */
-	ws.onmessage = async msg => {
+	ws.addEventListener('message', msg => {
 		const data: Nano.WebSocket = JSON.parse(msg.data)
 		if (data.message.block.subtype != 'receive') return
 
-		try {
-			this.processTransaction(data)
-		} catch (err) {
+		this.processTransaction(data).catch(err => {
 			console.error('Error while processing socket message', err)
-		}
-	}
+		})
+	})
 
-	ws.onerror = (event: any) => {
-		if (event.error.code === 'ECONNREFUSED' ||
-			event.error.code === 'ECONNRESET'
-		) {
+	ws.addEventListener('error', event => {
+		const error = event.error as AxiosError
+		if (error.code === 'ECONNREFUSED' || error.code === 'ECONNRESET') {
 			if (firstConnection) {
 				/**
 				 * Não emite rpc_disconnected caso seja a primeira conexão
@@ -71,5 +61,5 @@ export function nanoWebSocket(this: Nano) {
 		} else {
 			console.error('WebSocket error observed:', event)
 		}
-	}
+	})
 }
